25fd3aa26dd533682fce661d0b140ccc7a8dffb9
kamontat <kamontat_c@hotmail.com>
2017-01-26 16:06:23 +0700

    change description

--- src/main/java/constant/RequestStatus.java
+++ src/main/java/constant/RequestStatus.java
@@ -6,11 +6,15 @@ package constant;
  * @since 1/26/2017 AD - 2:11 PM
  */
 public enum RequestStatus {
-	COMPLETE("Download complete!"), EMPTY("Empty issue."),
+	COMPLETE("Download complete!"),
+	EMPTY("Empty issue."),
 	
-	FILE_ERROR("Cannot create csv file!"), REPO_NOT_FOUND("Repository not found."),
+	FILE_ERROR("Cannot create csv file!"),
+	REPO_NOT_FOUND("Repository not found."),
 	
-	INTERNET_LOST("Internet lost!"), RATE_EXCEED("Rate limit exceed."), USER_NOT_FOUND("Username not found."), ERROR("An Exception has occurred!");
+	INTERNET_ERROR("4xx/5xx http-status-code"),
+	RATE_EXCEED("Rate limit exceed."),
+	USER_NOT_FOUND("Username not found."), ERROR("An Exception has occurred!");
 	
 	public String description;
 	
@@ -23,7 +27,7 @@ public enum RequestStatus {
 	}
 	
 	public boolean haveUser() {
-		return !isError() && this != INTERNET_LOST && this != RATE_EXCEED && this != USER_NOT_FOUND && this != ERROR;
+		return !isError() && this != INTERNET_ERROR && this != RATE_EXCEED && this != USER_NOT_FOUND && this != ERROR;
 	}
 	
 	public boolean haveRepo() {
---
+++ src/main/java/model/Repository.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since 1/26/2017 AD - 3:27 PM
+ */
+public class Repository {
+}
