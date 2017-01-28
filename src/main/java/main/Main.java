c1fa12330ea73dbb0205fdc67d43158ded520192
Soraya Saenna <bubblebitoey@gmail.com>
2017-01-28 23:36:44 +0700

    remove test in main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -18,21 +18,6 @@ public class Main {
 			System.out.println(account.user.url);
 			System.out.println(account.getIssueCSV(repository_name, GHIssueState.OPEN));
 		}
-		
-		// cache test
-//		Cache.loadCache().setFileName("hello-world").saveToFile(GithubToken.getGitToken("a", "asdffdsa"));
-//		GithubToken x = Cache.loadCache().setFileName("hello-world").loadFromFile(GithubToken.class);
-//		System.out.println(x);
-		
-		// encryption test
-//		String pass = "kamontat";
-//		String text = "I fuck you";
-//		Encryption encryption = Encryption.get(pass);
-//
-//		String code = encryption.encode(pass, text);
-//		System.out.println(text.equals(code));
-//
-//		String text2 = encryption.decode(pass, code);
-//		System.out.println(text.equals(text2));
 	}
 }
+
