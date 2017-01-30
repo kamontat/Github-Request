d9cf6cb99c65d39f733fc71f6ebf9b5491d83cdf
kamontat <kamontat_c@hotmail.com>
2017-01-31 05:01:17 +0700

    add more controller - device - controller about device - popup log - output to user

---
+++ src/main/java/com/kamontat/controller/Device.java
@@ -0,0 +1,23 @@
+package com.kamontat.controller;
+
+import java.awt.*;
+import java.net.URL;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since 1/31/2017 AD - 4:33 AM
+ */
+public class Device {
+	private static Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop(): null;
+	
+	public static void openWeb(URL url) {
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				desktop.browse(url.toURI());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
