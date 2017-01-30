d3280d98cf33cc9d2c877a8ec082d802d27d2c38
kamontat <kamontat_c@hotmail.com>
2017-01-30 14:00:13 +0700

    add location and size management

---
+++ src/main/java/com/kamontat/gui/code/Location.java
@@ -0,0 +1,49 @@
+package com.kamontat.gui.code;
+
+import java.awt.*;
+
+/**
+ * @author kamontat
+ * @version 4.1
+ * @since 17/8/59 - 23:54
+ */
+public class Location {
+	
+	/**
+	 * get default point at x=0, y=0
+	 */
+	public static Point getDefaultPoint = new Point(0, 0);
+	
+	/**
+	 * get point that stay in the center of the screen
+	 *
+	 * @param pageSize
+	 * 		size of page that want to show in the center
+	 * @return point of center screen (you can set in <code>setLocation()</code> directly)
+	 */
+	public static Point getCenterLocation(Dimension pageSize) {
+		return new Point((int) ((Size.getScreenSize().getWidth() / 2) - (pageSize.getWidth() / 2)), (int) ((Size.getScreenSize().getHeight() / 2) - (pageSize.getHeight() / 2)));
+	}
+	
+	/**
+	 * get point to show page at center of the old page <br>
+	 * if don't have old page (oldPage is <code>null</code>) this method will return center of the screen
+	 *
+	 * @param oldPage
+	 * 		current page
+	 * @param newPage
+	 * 		new page that want to show
+	 * @return point (you can set in <code>setLocation()</code> directly)
+	 */
+	public static Point getCenterPage(Window oldPage, Window newPage) {
+		if (oldPage == null) {
+			return getCenterLocation(newPage.getSize());
+		}
+		Point currPoint = oldPage.getLocation();
+		
+		int newX = (currPoint.x + (oldPage.getWidth() / 2)) - (newPage.getWidth() / 2);
+		int newY = (currPoint.y + (oldPage.getHeight() / 2)) - (newPage.getHeight() / 2);
+		
+		return new Point(newX, newY);
+	}
+}
---
+++ src/main/java/com/kamontat/gui/code/Size.java
@@ -0,0 +1,56 @@
+package com.kamontat.gui.code;
+
+import java.awt.*;
+
+/**
+ * @author kamontat
+ * @version 1.2
+ * @since 1/30/2017 AD - 1:47 PM
+ */
+public class Size {
+	/**
+	 * Screen information <br>
+	 * - get size <code>screen.getWidth</code> and <code>screen.getHeight</code>
+	 */
+	private static DisplayMode screen = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
+	
+	/**
+	 * get Screen size
+	 *
+	 * @return screen size
+	 */
+	public static Dimension getScreenSize() {
+		return new Dimension(screen.getWidth(), screen.getHeight());
+	}
+	
+	/**
+	 * test than pageSize is bigger than screen (computer monitor) or not
+	 *
+	 * @param pageSize
+	 * 		page size (if you fixed size)
+	 * @return true if bigger than, otherwise will return false
+	 */
+	public static boolean isBiggerThanScreen(Dimension pageSize) {
+		return !(pageSize.getWidth() < screen.getWidth() && pageSize.getHeight() < screen.getHeight());
+	}
+	
+	/**
+	 * return the minimum width and height in a and b <br>
+	 * example: a = (200, 400), b = (500, 100) <br>
+	 * the result will be (500, 200)
+	 *
+	 * @param a
+	 * 		first size
+	 * @param b
+	 * 		second size
+	 * @return the minimum size
+	 */
+	public static Dimension maximum(Dimension a, Dimension b) {
+		Dimension result = new Dimension();
+		if (a.getWidth() > b.getWidth()) result.setSize(a.getWidth(), result.getHeight());
+		else result.setSize(b.getWidth(), result.getHeight());
+		if (a.getHeight() > b.getHeight()) result.setSize(result.getWidth(), a.getHeight());
+		else result.setSize(result.getWidth(), b.getHeight());
+		return result;
+	}
+}
