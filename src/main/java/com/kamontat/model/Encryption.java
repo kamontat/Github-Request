647e0a48c0648f54aef02df367e7e2978710abfe
kamontat <kamontat_c@hotmail.com>
2017-02-01 15:12:44 +0700

    [Encryption, GithubToken] add javadoc

--- src/main/java/com/kamontat/model/Encryption.java
+++ src/main/java/com/kamontat/model/Encryption.java
@@ -6,41 +6,92 @@ import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import java.io.Serializable;
 
 /**
+ * This class use library from <b>jasypt</b> to encryption
+ *
  * @author kamontat
  * @version 1.2
+ * @see <a href="www.jasypt.com">jasypt</a>
  * @since 1/28/2017 AD - 2:34 AM
  */
 public class Encryption implements Serializable {
-	private static final String DEFAULT_PASSWORD = "Hello-World";
 	static final long serialVersionUID = 1L;
 	
-	private StandardPBEStringEncryptor stringEncryptor;
+	private StandardPBEStringEncryptor encryption;
 	
+	/**
+	 * decrypt the code by using real password <br>
+	 * and this method will change your real password to some unknown password later <br>
+	 * so you can't decrypt the code by yourself <br>
+	 *
+	 * @param rawPass
+	 * 		real password to decrypt
+	 * @param code
+	 * 		text code
+	 * @return normal text
+	 * @throws Exception
+	 * 		mostly throws when password was wrong, but there have another exception else
+	 */
 	public static String decode(String rawPass, String code) throws Exception {
-		return new Encryption(getPassword(rawPass)).decode(code);
+		return new Encryption(getAdvancedEncryption(rawPass)).decode(code);
 	}
 	
-	public static String encode(String rawPass, String code) throws Exception {
-		return new Encryption(getPassword(rawPass)).encode(code);
+	/**
+	 * encrypt the text by using real password <br>
+	 * and this method will change your real password to some unknown password later <br>
+	 * so if you direct encode the password, you shouldn't use <code>decode</code> method in this class
+	 *
+	 * @param rawPass
+	 * 		real password to encrypt
+	 * @param text
+	 * 		normal text
+	 * @return encrypt code
+	 * @throws Exception
+	 * 		every exception that occurred
+	 */
+	public static String encode(String rawPass, String text) throws Exception {
+		return new Encryption(getAdvancedEncryption(rawPass)).encode(text);
 	}
 	
-	private static Encryption get(String rawPass) {
-		return new Encryption(getPassword(rawPass));
-	}
-	
+	/**
+	 * save password into encryption to decode and encode later
+	 *
+	 * @param password
+	 * 		real password
+	 */
 	private Encryption(String password) {
 		configEncryption(password);
 	}
 	
+	/**
+	 * decrypt the text by using password
+	 *
+	 * @param code
+	 * 		encrypt text
+	 * @return normal text
+	 */
 	private String decode(String code) {
-		return stringEncryptor.decrypt(code);
+		return encryption.decrypt(code);
 	}
 	
+	/**
+	 * encrypt the text by using password
+	 *
+	 * @param text
+	 * 		normal text
+	 * @return encrypt text
+	 */
 	private String encode(String text) {
-		return stringEncryptor.encrypt(text);
+		return encryption.encrypt(text);
 	}
 	
-	private static String getPassword(String pass) {
+	/**
+	 * encryption pass by using advanced encryption
+	 *
+	 * @param pass
+	 * 		text or password to encrypt
+	 * @return advanced encrypt string
+	 */
+	private static String getAdvancedEncryption(String pass) {
 		StandardStringDigester p = new StandardStringDigester();
 		p.setSaltGenerator(NoSaltGenerator.get());
 		p.setAlgorithm("SHA-512");
@@ -48,10 +99,20 @@ public class Encryption implements Serializable {
 		return p.digest(pass);
 	}
 	
+	/**
+	 * config encryption
+	 * current config is
+	 * <ul>
+	 * <li>output as HEX string</li>
+	 * </ul>
+	 *
+	 * @param pass
+	 * 		encryption must have password to encrypt or decrypt
+	 */
 	private void configEncryption(String pass) {
-		stringEncryptor = new StandardPBEStringEncryptor();
-		stringEncryptor.setStringOutputType("HEX");
-		stringEncryptor.setPassword(pass);
+		encryption = new StandardPBEStringEncryptor();
+		encryption.setStringOutputType("HEX");
+		encryption.setPassword(pass);
 	}
 	
 }
--- src/main/java/com/kamontat/server/GithubToken.java
+++ src/main/java/com/kamontat/server/GithubToken.java
@@ -8,43 +8,73 @@ import java.io.IOException;
 import java.io.Serializable;
 
 /**
+ * this class very importance in <code>GithubLoader</code> class <br>
+ * this will use to sign in into github by using Token
+ * Normally when you login, you need to enter username and password but to authentication, you can use token as the same way as username and password.
+ * you can get easy your token by login your github and go to this link
+ * <ul>
+ * <li><code>https://github.com/settings/tokens/new</code> - To generate new token</li>
+ * <li><code>https://github.com/settings/tokens</code> - To manage your exist token</li>
+ * </ul>
+ * and this class also use cache to keep your encrypt token pass using <code>Encryption</code> class
+ *
  * @author kamontat
  * @version 2.2
+ * @see GithubLoader
+ * @see java.io.Serializable
+ * @see Cache
+ * @see Encryption
  * @since 1/28/2017 AD - 12:51 AM
  */
 public class GithubToken implements Serializable {
-	static final long serialVersionUID = 1L;
+	// stand for version
+	static final long serialVersionUID = 2L;
+	// default name of cache file
 	private static final String FILE_NAME = "Token";
+	// token
 	private String token;
 	
-	// if have token rate_limit will be `5000`, otherwise rate_limit will be `60`
-	
 	/**
-	 * this method must destroy before put to public
+	 * return empty token
 	 *
-	 * @return null
+	 * @return empty token
 	 */
-	@Deprecated
-	public static GithubToken getDefaultToken() {
-		return new GithubToken("925cc49f2798daae39b0e594896fdea9388e528f");
+	private static GithubToken getEmptyToken() {
+		return new GithubToken(null);
 	}
 	
+	/**
+	 * constructor that will create token by parameter
+	 *
+	 * @param token
+	 * 		github token
+	 */
 	public GithubToken(String token) {
 		this.token = token;
 	}
 	
-	public static GithubToken getEmptyToken() {
-		return new GithubToken(null);
-	}
-	
+	/**
+	 * reset token to empty
+	 */
 	public void resetToken() {
 		token = null;
 	}
 	
+	/**
+	 * get real token or <b>empty string</b> ("") if it's <b>empty token</b>
+	 *
+	 * @return token
+	 */
 	public String getToken() {
 		return token == null ? "": token;
 	}
 	
+	/**
+	 * check is current token valid or not <b>(using internet)</b> <br>
+	 * this method may be slow
+	 *
+	 * @return true if token valid; false otherwise
+	 */
 	public boolean isTokenValid() {
 		try {
 			GitHub.connectUsingOAuth(token);
@@ -54,15 +84,40 @@ public class GithubToken implements Serializable {
 		}
 	}
 	
+	/**
+	 * encrypt current github token by using password
+	 *
+	 * @param password
+	 * 		encrypt password
+	 * @return Encrypt GithubToken
+	 * @throws Exception
+	 * 		every exception that occurred
+	 */
 	private GithubToken encryptGT(String password) throws Exception {
 		return new GithubToken(Encryption.encode(password, token));
 	}
 	
+	/**
+	 * decrypt current github token by using password
+	 *
+	 * @param password
+	 * 		decrypt password
+	 * @return Normal GithubToken
+	 * @throws Exception
+	 * 		mostly will throw if password wrong
+	 */
 	private GithubToken decryptGT(String password) throws Exception {
 		this.token = Encryption.decode(password, token);
 		return this;
 	}
 	
+	/**
+	 * save current github token into cache file
+	 *
+	 * @param password
+	 * 		password that need to decryption later
+	 * @return true if save successfully; false otherwise
+	 */
 	public boolean saveCache(String password) {
 		try {
 			Cache.loadCache(FILE_NAME).saveToFile(this.encryptGT(password));
@@ -72,28 +127,52 @@ public class GithubToken implements Serializable {
 		}
 	}
 	
+	/**
+	 * load github token from cache file by using password that create when save cache
+	 *
+	 * @param password
+	 * 		decryption password
+	 * @return real token if password correctly; false otherwise
+	 */
 	public static GithubToken loadCache(String password) {
 		try {
 			if (Cache.loadCache(FILE_NAME).isExist())
 				return Cache.loadCache(FILE_NAME).loadFromFile(GithubToken.class).decryptGT(password);
-			else return GithubToken.getEmptyToken();
-		} catch (Exception e) {
-			return GithubToken.getEmptyToken();
+		} catch (Exception ignored) {
 		}
+		return GithubToken.getEmptyToken();
 	}
 	
+	/**
+	 * delete token cache if it exist
+	 */
 	public static void removeCache() {
-		if (Cache.loadCache(FILE_NAME).isExist()) Cache.loadCache(FILE_NAME).delete();
+		if (haveCache()) Cache.loadCache(FILE_NAME).delete();
 	}
 	
+	/**
+	 * check is current token is empty or not
+	 *
+	 * @return true if empty; otherwise return false
+	 */
 	public boolean isEmptyToken() {
 		return token == null;
 	}
 	
+	/**
+	 * check is have token cache or not
+	 *
+	 * @return true if have cache, false in otherwise
+	 */
 	public static boolean haveCache() {
 		return Cache.loadCache(FILE_NAME).isExist();
 	}
 	
+	/**
+	 * return string how to create new token
+	 *
+	 * @return helping create token string
+	 */
 	public static String getHelp() {
 		return "create new one at https://github.com/settings/tokens/new/";
 	}
